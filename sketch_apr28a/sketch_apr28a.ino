f2c36b6be8ef41c5540b8d6d033a4aa0cea0abc3
Alexander Song <88013429+ptatoo@users.noreply.github.com>
2026-05-12 07:55:24 -0700

    pre week 7

--- sketch_apr28a/sketch_apr28a.ino
+++ sketch_apr28a/sketch_apr28a.ino
@@ -1,24 +1,35 @@
 #include <ECE3.h>
+#include "PIDController.h"
 String dummy;
 
 uint16_t sensorValues[8];
-
+void donut();
 //Sensor Fusion Values
 int minValues[] = {805, 664, 665, 573, 664, 687, 735, 804};
 int maxValues[] = {2500, 2500, 2500, 1514, 2107, 2500, 2500, 2500};
 int weights[] = {-15, -14, -12, -8, 8, 12, 14, 15};
 int divideNum = 8;
-
 int number_samples = 5;
-int motorNotSleep = false;
-int speed = 20;
 
-const int left_nslp_pin=31; // nslp ==> awake & ready for PWM
+//motor values
+int motorNotSleep = false;
+int initialSpeed = 100;
+int minSpeed = 0;
+int maxSpeed = 150;
+
+int leftSpeed = initialSpeed;
+int rightSpeed = initialSpeed;
+
+//motor pin values
+const int left_nslp_pin=31;
 const int left_dir_pin=29;
 const int left_pwm_pin=40;
 const int right_nslp_pin = 11;
 const int right_dir_pin = 30;
 const int right_pwm_pin = 39;
+
+PIDController leftPID = PIDController(0.01, 0.000001, 0.0001, 0);
+PIDController rightPID = PIDController(1, 0, 0, 0);
 
 //hehe red amogus sus :)
 void setup()
@@ -26,7 +37,7 @@ void setup()
   ECE3_Init();
   Serial.begin(9600); // set the data rate in bits per second for serial data transmission
   delay(1000);
-  
+ 
   pinMode(left_nslp_pin,OUTPUT);
   pinMode(left_dir_pin,OUTPUT);
   pinMode(left_pwm_pin,OUTPUT);
@@ -42,28 +53,15 @@ void setup()
   digitalWrite(right_nslp_pin,HIGH);
 }
 
-
 void loop()
 {
-  //wait for user input
-  String input = Serial.readString();
-  if (input == "\n") {
-    motorNotSleep = !motorNotSleep;
-    if (motorNotSleep) {
-      digitalWrite(right_nslp_pin,HIGH);
-      digitalWrite(left_nslp_pin,HIGH);
-    } else {
-      digitalWrite(right_nslp_pin,LOW);
-      digitalWrite(left_nslp_pin,LOW);
-    }
-  }
 
-  analogWrite(left_pwm_pin, speed);
-  analogWrite(right_pwm_pin, speed);
-  
+
+  // analogWrite(left_pwm_pin, leftSpeed);
+  // analogWrite(right_pwm_pin, rightSpeed);
   int summed_values[8] = {0};
   int normalizedValues[8] = {0};
-  float weightedSum = 0;
+  int weightedSum = 0;
 
   // Take the average of 5 consecutive values for each sensor
   for (int j = 0; j < number_samples; j++){
@@ -76,22 +74,53 @@ void loop()
       summed_values[i] += sensorValues[i];
     }
   }
-
-  // Print current position at start of each column
-  Serial.print("\t");
-
+  int turnAroundSum;
   // Print average values (average value = summed_values / number_samples
   for (unsigned char i = 0; i < 8; i++) {
     int avg = summed_values[i] / number_samples;
-    float normalizedValue= (summed_values[i] / number_samples - minValues[i])*(1.0)/(maxValues[i] - minValues[i]) * 1000.0;
+    int normalizedValue= (summed_values[i] / number_samples - minValues[i])*(1.0)/(maxValues[i] - minValues[i]) * 1000.0;
     weightedSum += normalizedValue * weights[i] / divideNum;
+    turnAroundSum += normalizedValue / divideNum;
 
-    Serial.print(normalizedValue);
-    Serial.print('\t');
+    //Serial.print(normalizedValue);
+    //Serial.print('\t');
   }
-  //Serial.print(weightedSum);
+  Serial.print(turnAroundSum);
+  Serial.print(" | ");
+  Serial.print(weightedSum);
+  Serial.print(" | ");
+  int  PIDNumber = leftPID.calculate(weightedSum);
+Serial.print(PIDNumber);
+  Serial.print(" | ");
+  Serial.print(leftSpeed);
+  Serial.print(" | ");
+  Serial.print(rightSpeed);
   Serial.println();
 
-  // Increment current position by the increment value
+  leftSpeed = initialSpeed + (PIDNumber);
+  rightSpeed = initialSpeed - (PIDNumber);
+  if (turnAroundSum >= 1000)
+  {
+    donut(turnAroundSum);
+  }
+  
+
+
+  leftSpeed  = constrain(leftSpeed, minSpeed, maxSpeed);
+  rightSpeed = constrain(rightSpeed, minSpeed, maxSpeed);
+  delay(5);
+
+}
+
+void donut(int &turnAroundSum)
+{
+  while(turnAroundSum >= 1000)
+  {
+  analogWrite(left_pwm_pin, 90);
+  analogWrite(right_dir_pin , HIGH);
+  analogWrite(right_pwm_pin, 90);
   delay(10);
+  }
+  analogWrite(right_dir_pin , LOW);
+
 }
